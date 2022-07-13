a594aa74baeb2f1e6efc5d4fa73e169222aa1d0c
sanjaybiswas601 <sanjaybiswas601@gmail.comgit config --global user.name sanjaybiswas601Omit --globalgit config --global user.email sanjaybiswas601@gmail.com>
2022-07-13 17:25:38 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import logo from './logo.svg';
+import React,{useState} from 'react'
 import './App.css';
 
 function App() {
+  const [data,setData] = useState([
+    {id:1,username:"sanjay1"},
+    {id:2,username:"sanjay2"},
+    {id:3,username:"sanjay3"},
+    {id:4,username:"sanjay4"},
+    {id:5,username:"sanjay5"}
+  ])
+
+  const handlelevel = (id)=>{
+    const newData = data.filter(data=>data.id!==id)
+    setData(newData)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1>hi everybody</h1>
+      {
+        data.map((data) =>
+          <div>{data.username}<button onClick={() => handlelevel(data.id)}>delete</button></div>
+        )
+      }
+    </>
   );
 }
 
